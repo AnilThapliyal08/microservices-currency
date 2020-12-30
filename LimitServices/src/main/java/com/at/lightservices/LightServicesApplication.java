0dcc031f9cd9da1c15533f2e22a2b72d847671a7
Anil Thapliyal <anilthapliyal08@gmail.com>
2020-12-30 13:33:41 +0530

    Config server

---
+++ LimitServices/src/main/java/com/at/lightservices/LightServicesApplication.java
@@ -0,0 +1,13 @@
+package com.at.lightservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightServicesApplication.class, args);
+	}
+
+}
