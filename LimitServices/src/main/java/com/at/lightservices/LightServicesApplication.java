3d490b775e2cca7b2c4d0a1ca15e46525885320c
Anil Thapliyal <anilthapliyal08@gmail.com>
2020-12-30 22:35:35 +0530

    Final checkin

--- LimitServices/src/main/java/com/at/lightservices/LightServicesApplication.java
+++ LimitServices/src/main/java/com/at/lightservices/LightServicesApplication.java
@@ -2,8 +2,10 @@ package com.at.lightservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 public class LightServicesApplication {
 
 	public static void main(String[] args) {
--- LimitServices/src/main/java/com/at/lightservices/LimitServiceController.java
+++ LimitServices/src/main/java/com/at/lightservices/LimitServiceController.java
@@ -4,13 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class LimitServiceController {
 
 	@Autowired
 	private Configuration configuration;
 	@GetMapping("/limits")
+	@HystrixCommand
 	public LimitConfiguration getLimitsFromConfigurations() {
 		return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
 	}
+	
+	@GetMapping("/fault-tolerance")
+	@HystrixCommand(fallbackMethod = "fallbackMethod" )
+	public LimitConfiguration retriveConfiguration() {
+		throw new RuntimeException("Not available");
+	}
+	
+	public LimitConfiguration fallbackMethod() {
+		return new LimitConfiguration(55,5);
+	}
+	
+	
 }
